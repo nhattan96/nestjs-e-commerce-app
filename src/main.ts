9efcb124b1fd83d38f95038f28d5ab5550742abd
nhattan96 <tranhuynhnhattan96@gmail.com>
2026-08-05 14:57:26 +0700

    (update): Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce APIs')
+    .setDescription('The e-commerce API description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  // Start the server
   await app.listen(process.env.PORT ?? 3000);
+  console.log('Server is running on port', process.env.PORT ?? 3000);
 }
 bootstrap();
