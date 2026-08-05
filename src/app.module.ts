ea1b8dfc627147245aba68d360842720ecdfdef6
nhattan96 <tranhuynhnhattan96@gmail.com>
2026-08-05 15:44:11 +0700

    (update): init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, OrdersModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
