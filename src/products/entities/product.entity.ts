14eb3408a7c9046ebe275b70a087940d768dccda
nhattan96 <tranhuynhnhattan96@gmail.com>
2026-08-10 22:22:09 +0700

    (update): product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,28 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column('text')
+  name!: string;
+
+  @Column('text')
+  description!: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  price!: number;
+
+  @Column('text')
+  image!: string;
+
+  @Column('simple-json', { default: {} })
+  specs!: Record<string, string>;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  createdAt!: Date;
+
+  @Column('timestamp', { default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt!: Date;
+}
