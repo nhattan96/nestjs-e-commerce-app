6b2f8eabaaa86208722be28d8e8e27fb2fe4629d
nhattan96 <tranhuynhnhattan96@gmail.com>
2026-08-10 22:05:20 +0700

    (feat): postgre config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
 import { PaymentsModule } from './payments/payments.module';
+import { PostgreTypeOrmModule } from './databases/postgre/postgre.typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, UsersModule, OrdersModule, PaymentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    PostgreTypeOrmModule,
+
+    ProductsModule,
+    UsersModule,
+    OrdersModule,
+    PaymentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/databases/postgre/postgre.typeorm.config.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const PostgreTypeOrmModule = TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+    const config: TypeOrmModuleOptions = {
+      type: 'postgres',
+      host: configService.get<string>('POSTGRE_HOST', 'localhost'),
+      port: parseInt(configService.get<string>('POSTGRE_PORT', '5432'), 10),
+      username: configService.get<string>('POSTGRE_USERNAME'),
+      password: configService.get<string>('POSTGRE_PASSWORD'),
+      database: configService.get<string>('POSTGRE_DATABASE'),
+      synchronize: true,
+    };
+
+    console.log('PostgreSQL connected !');
+
+    return config;
+  },
+});
